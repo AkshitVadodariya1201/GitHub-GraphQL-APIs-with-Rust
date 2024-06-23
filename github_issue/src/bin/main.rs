3e957a6f0c7a9d5598f6b9f31adb82d9985d4c79
Akshit Vadodariya <108290138+AkshitVadodariya1201@users.noreply.github.com>
2024-06-23 06:51:25 -0400

    add comments for readability (#4)

--- github_issue/src/bin/main.rs
+++ github_issue/src/bin/main.rs
@@ -8,11 +8,13 @@ use async_graphql_rocket::GraphQLResponse;
 use github_issue::handler::{mutation::Mutation, query::Query, ProjectSchema};
 use rocket::{response::content, routes, State};
 
+/// Create a route for the GraphQL query
 #[rocket::get("/graphql?<query..>")]
 pub async fn graphql_query(schema: &State<ProjectSchema>, query: GraphQLQuery) -> GraphQLResponse {
     query.execute(schema.inner()).await
 }
 
+// Create a route for the GraphQL mutation
 #[rocket::post("/graphql", data = "<request>", format = "application/json")]
 pub async fn graphql_mutation(
     schema: &State<ProjectSchema>,
@@ -21,14 +23,19 @@ pub async fn graphql_mutation(
     request.execute(schema.inner()).await
 }
 
+// Create a route for the GraphQL playground
 #[rocket::get("/")]
 pub async fn graphql_playground() -> content::RawHtml<String> {
     content::RawHtml(playground_source(GraphQLPlaygroundConfig::new("/graphql")))
 }
 
+// Launch the Rocket server
 #[rocket::launch]
 fn rocket() -> _ {
+    // Create a new schema
     let schema = Schema::build(Query, Mutation, EmptySubscription).finish();
+
+    // Mount the schema on the Rocket server
     rocket::build().manage(schema).mount(
         "/",
         routes![graphql_query, graphql_mutation, graphql_playground],
--- github_issue/src/schema/git_repo.rs
+++ github_issue/src/schema/git_repo.rs
@@ -1,6 +1,7 @@
 use async_graphql::{InputObject, SimpleObject};
 use serde::{Deserialize, Serialize};
 
+// Define the Repository struct
 #[derive(InputObject)]
 pub struct Repository {
     pub owner: String,
@@ -8,6 +9,7 @@ pub struct Repository {
     pub issuenumber: String,
 }
 
+// Define the RepositoryDetail struct
 #[derive(InputObject)]
 pub struct RepositoryDetail {
     pub owner: String,
@@ -15,11 +17,13 @@ pub struct RepositoryDetail {
     pub last: usize,
 }
 
+// Define the IssueQuery struct
 #[derive(Serialize)]
 pub struct IssueQuery {
     pub query: String,
 }
 
+// Define the CreateIssue struct
 #[derive(InputObject)]
 pub struct CreateIssue {
     pub title: String,
@@ -27,6 +31,7 @@ pub struct CreateIssue {
     pub body: String,
 }
 
+// Define the UpdateIssue struct
 #[derive(InputObject)]
 pub struct UpdateIssue {
     pub newtitle: Option<String>,
@@ -34,15 +39,19 @@ pub struct UpdateIssue {
     pub newbody: Option<String>,
 }
 
+// Define the DeleteIssue struct
 #[derive(Serialize, SimpleObject, Deserialize)]
 pub struct ClientMutationId {
     pub client_mutation_id: String,
 }
 
+// Define the DeleteIssue struct
 #[derive(InputObject)]
 pub struct DeleteIssue {
     pub issue_id: String,
 }
+
+// Define the FetchIssue struct
 #[derive(Serialize, SimpleObject, Deserialize, Debug)]
 pub struct Issue {
     pub id: Option<String>,
@@ -52,26 +61,31 @@ pub struct Issue {
     pub url: Option<String>,
 }
 
+// Define the IssueNode struct
 #[derive(Debug, Deserialize)]
 pub struct IssueNode {
     pub nodes: Vec<Issue>,
 }
 
+// Define the RepositoryIssue struct
 #[derive(Debug, Deserialize)]
 pub struct RepositoryIssue {
     pub issues: IssueNode,
 }
 
+// Define the GraphQLQuery struct
 #[derive(Debug, Serialize)]
 pub struct GraphQLQuery {
     pub query: String,
 }
 
+// Define the FetchIssue struct
 #[derive(InputObject)]
 pub struct FetchIssue {
     pub issue_id: String,
 }
 
+// Define the AddLabelsToLabelable struct
 #[derive(InputObject, Serialize, Debug)]
 pub struct AddLabelsToLabelable {
     pub issue_id: String,
